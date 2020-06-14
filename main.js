801d5d11f50fa047f48d1cd585972817f8d07719
bjhaj <64169433+bjhaj@users.noreply.github.com>
2020-06-13 18:24:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
-async function bruh() {
-const model= await tf.loadLayersModel('https://raw.githubusercontent.com/bjhaj/Image-Classifer-Project/master/model/model.json');
-//https://cors-anywhere.herokuapp.com/https://github.com/bjhaj/Image-Classifer-Project/blob/master/model/model.json
+/*function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
 
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
 }
-
-bruh();
+*/
 var class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'];
 
+/* 
 function upload() {
-	var imgcanvas = document.getElementById("can");	
 	var fileinput = document.getElementById("finput");
 	var filename = fileinput.value;
 	alert("Chose " + filename);
 	var img= new SimpleImage(fileinput);
-	img.drawTo(imgcanvas);
+}
+var img1 = new ImageData(1,1); // Image constructor
+img1.src = 'image.png';
+img1.alt = 'alt';
+document.body.appendChild(img1);
+*/
+var img2 = new Image(); // Use DOM HTMLImageElement
+img2.crossOrigin = "anonymous";
+//img2.src = 'https://i.imgur.com/z4QGm6b.jpg';
+//img2.src = 'https://3qeqpr26caki16dnhd19sv6by6v-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/sample_image-300x300.png';
+img2.src = 'https://i.imgur.com/p3Dc6KQ.jpg';
+
+function preprocess(img) {
+	const tensor = tf.browser.fromPixels(img,1); 	
+	console.log(tensor.shape); 
+	let resized = tf.image.resizeBilinear(tensor,[28,28]).toFloat();
+	 resized=tf.expandDims(resized, axis=0)	
+	console.log(img.shape);
+	return resized;
+
+ 
 }
 
-function preprocess(img)
-{
-
-    //convert the image data to a tensor  
-    let tensor = tf.fromPixels(img)
-    //resize to 28 X 28
-    const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
-    // Normalize the image 
-    const offset = tf.scalar(255.0);
-    const normalized = tf.scalar(1.0).sub(resized.div(offset));
-    //We add a dimension to get a batch shape 
-    const batched = normalized.expandDims(0)
-    return batched
-
+async function bruh() {
+const model= await tf.loadLayersModel('https://raw.githubusercontent.com/bjhaj/Image-Classifer-Project/master/model/model.json');
+const predictions_single = model.predict(preprocess(img2));
+console.log((predictions_single));
+document.write(predictions_single);
+//document.write(class_names[indexOfMax(predictions_single)]);
 }
+bruh();
 
-//write the probabbility model using the input from the batch images, then have it display the output, goal for tommorow is to get the percent down, eventually lets get it to produce a graph
-
-//const probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()]);
